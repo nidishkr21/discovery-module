860dec3782b62ae4107d49c7dfd79042c7bacf4f
Nidish Ramakrishnan <nramakrishnan@grepruby.com>
2018-12-03 11:30:48 +0530

    Chore: Modularize csv file format

--- extract-json/convert.rb
+++ extract-json/convert.rb
@@ -18,16 +18,8 @@ records.each do |record|
     puts('Running through row')
     puts(index)
     File.open("data/#{filename}.json", 'a') do |w|
-    newRecord = record
-    newRecord = {ID: index}.merge(record)
-    newRecord[:user_id] = newRecord[:user_id].to_s
-    newRecord[:occupation] = newRecord[:occupation].to_s
-    newRecord[:stay_in_current_city_years] = newRecord[:stay_in_current_city_years].to_s;
-    newRecord[:marital_status] = newRecord[:marital_status].to_s;
-    newRecord[:product_category_1] = newRecord[:product_category_1].to_s;
-    newRecord[:product_category_2] = newRecord[:product_category_2].to_s;
-    newRecord[:product_category_3] = newRecord[:product_category_3].to_s;
-    newRecord[:purchase] = newRecord[:purchase].to_s;
+    newRecord = record.map { |k, str| [k, record[k].to_s] }.to_h
+    newRecord = {ID: index}.merge(newRecord)
     w.puts JSON.pretty_generate(newRecord)
   end
 end
